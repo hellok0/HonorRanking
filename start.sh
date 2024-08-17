d1c8d8ae02f557043a55f0b3797210652608257a
Glitch (glitch-hello-node) <none>
2024-08-17 10:43:18 +0000

    🐛☕️ Checkpoint ./start.sh:69393092/921 ./requirements.txt:69393092/296

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
-# Run the server script
-python server.py
+#!/bin/sh
+
+# Install Python 3 and pip
+apt-get update
+apt-get install -y python3 python3-pip
+
+# Install dependencies
+pip3 install -r requirements.txt
+
+# Run the server
+python3 server.py
